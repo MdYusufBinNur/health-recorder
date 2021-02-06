021f916fa7ba62350f0c9e100f5b5f95c8e67e53
MdYusufBinNur <engr.mdyusuf@gmail.com>
2021-02-06 19:43:21 +0600

    Header Modified

--- app/Http/Middleware/ModifyHeader.php
+++ app/Http/Middleware/ModifyHeader.php
@@ -1,12 +1,8 @@
 <?php
-​
 namespace App\Http\Middleware;
-​
-use App\Services\Helpers\IdHashingHelper;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Config;
-​
+
 class ModifyHeader
 {
     /**
@@ -21,45 +17,14 @@ class ModifyHeader
         if (empty($request->headers->get('Content-Type'))) {
             $request->headers->set('Content-Type', 'application/json');
         }
-        ​
         if ($request->headers->get('Accept') == '*/*') {
             $request->headers->set('Accept', 'application/json');
         }
-​
-//        $propertyId = $request->headers->has('propertyId') ? $request->headers->get('propertyId') : $request->get('propertyId');
-//        if (!empty($propertyId)) {
-//
-//            $request->merge(['propertyId' => $propertyId]);
-//            $property = Property::find($propertyId);
-//
-//            \View::share('property', $property);
-//        }
-​
-​
-        foreach ($request->all() as $key => $value) {
-            if ((strpos($key, 'Id') || $key == 'id') && !is_numeric($value) && !is_null($value)) {
-                $request->merge([$key => IdHashingHelper::decode($value)]);
-            }
-            if ($key == 'products') {
-                $newProducts = [];
-                foreach ($value as $ke => $product) {
-                    foreach ($product as $k => $v) {
-                        if (strpos($k, 'Id') && !is_numeric($v)) {
-                            $product[$k] = IdHashingHelper::decode($v);
-                        }
-                    }
-                    $newProducts[$ke] = $product;
-                }
-                $request->merge([$key => $newProducts]);
-            }
-        }
-​
         $response = $next($request);
         $response->headers->set("Access-Control-Allow-Origin", "*");
         $response->headers->set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Requested-With");
         $response->headers->set("Access-Control-Allow-Methods", "OPTIONS, HEAD, GET, POST, PUT, DELETE");
         $response->headers->set("CrossOrigin", "Anonymous");
-​
         return $response;
     }
 }
